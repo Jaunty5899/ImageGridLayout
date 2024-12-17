17c090c6233fd87c7b453ea0cd2774fb9201614c
Jaunty <jaunty5899@gmail.com>
2024-12-17 14:28:50 +0530

    images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <img src="https://picsum.photos/seed/1/200/300" alt="" />
+      <img src="https://picsum.photos/seed/2/200/200" alt="" />
+      <img src="https://picsum.photos/seed/3/200/500" alt="" />
+      <img src="https://picsum.photos/seed/4/200/100" alt="" />
+      <img src="https://picsum.photos/seed/5/200/300" alt="" />
+      <img src="https://picsum.photos/seed/6/200/400" alt="" />
+      <img src="https://picsum.photos/seed/7/200/300" alt="" />
+      <img src="https://picsum.photos/seed/8/200/300" alt="" />
+    </div>
+  );
 }
 
 export default App;
